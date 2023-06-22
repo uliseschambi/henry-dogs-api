b12e702d15785e992d379637a5887053635c8333
uliseschambi <uliseschambi@gmail.com>
2023-06-21 21:26:30 -0300

    docker

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log("Synchronized models.");
-  server.listen(80, () => {
-    console.log("Listening at 80."); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log("Listening at 3001."); // eslint-disable-line no-console
   });
 });
