ed2edf8989d3adcdb43a8628a3bf583dec695cfc
uliseschambi <uliseschambi@gmail.com>
2023-07-05 17:29:00 -0300

    db

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } =
   process.env;
 
 const sequelize = new Sequelize(
-  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -17,24 +17,24 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
-    (file) =>
-      file.indexOf(".") !== 0 &&
+    file =>
+      file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === ".js"
+      file.slice(-3) === '.js'
   )
-  .forEach((file) => {
+  .forEach(file => {
     modelDefiners.push(
-      require(path.join(__dirname, "/models", file))
+      require(path.join(__dirname, '/models', file))
     );
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
+let capsEntries = entries.map(entry => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
@@ -47,12 +47,12 @@ const { Dog, Temperament } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 Dog.belongsToMany(Temperament, {
-  through: "breed_temperament",
+  through: 'breed_temperament',
   timestamps: false,
   // foreignKey: 'breed_breedId',
 });
 Temperament.belongsToMany(Dog, {
-  through: "breed_temperament",
+  through: 'breed_temperament',
   timestamps: false,
   // foreignKey: 'temperament_temperamentId',
 });
