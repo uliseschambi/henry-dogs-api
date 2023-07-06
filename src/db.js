0a1547ee94ee36a7b4d557336064cada4aef2f13
uliseschambi <uliseschambi@gmail.com>
2023-07-05 21:58:12 -0300

    pool

--- src/db.js
+++ src/db.js
@@ -11,9 +11,8 @@ const sequelize = new Sequelize(
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     pool: {
-      max: 5,
+      max: 4,
       min: 0,
-      acquire: 30000,
       idle: 10000,
     },
   }
