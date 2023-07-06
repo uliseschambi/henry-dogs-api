e47eefa071c58851cef6724afdecdf1e5673900d
uliseschambi <uliseschambi@gmail.com>
2023-07-05 21:06:35 -0300

    db

--- src/db.js
+++ src/db.js
@@ -30,7 +30,7 @@ fs.readdirSync(path.join(__dirname, '/models'))
     );
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+// Definimos cada modelo pasando como argumento la instancia de sequelize
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
