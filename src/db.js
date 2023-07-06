a31fee50c3d8036b93c42296cdafe356552538ce
uliseschambi <uliseschambi@gmail.com>
2023-07-05 21:30:31 -0300

    pool

--- src/db.js
+++ src/db.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 const basename = path.basename(__filename);
