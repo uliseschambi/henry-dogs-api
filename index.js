18d82032ecd82c54951b4fffbfc412ea6b5cd65a
uliseschambi <uliseschambi@gmail.com>
2023-07-07 17:15:34 -0300

    port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log('Synchronized models.');
-  // server.listen(port, () => {
-  server.listen(port, '0.0.0.0', () => {
+  server.listen(port, () => {
+    // server.listen(port, '0.0.0.0', () => {
     console.log('Listening at ', port); // eslint-disable-line no-console
   });
 });
