acbadae7ea1f6a7926a83a67c1da3f6e7335480c
uliseschambi <uliseschambi@gmail.com>
2023-07-07 18:20:30 -0300

    alter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db');
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   console.log('Synchronized models.');
   server.listen(port, () => {
     // server.listen(port, '0.0.0.0', () => {
